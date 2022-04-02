c70d93287410d3915ee0256001e8e4481f2e2982
Ben <bgrowcott@hotmail.co.uk>
2022-04-02 13:57:45 +0100

    got seeds working with new models

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,11 +1,14 @@
-const seedUsers = require('./userSeeds.json');
+const userData = require('./userData.json');
 const { User, Forum, Comment } = require('../models');
 const sequelize = require('../config/connection');
+
+const forumData = require('./forumData.json');
+const commentData = require('./commentdata.json');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
-  const users = await User.bulkCreate(seedUsers, {
+  const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
