c47ca74dd156115921d4b42f4e672505e066bd3f
Ben <bgrowcott@hotmail.co.uk>
2022-04-02 11:33:38 +0100

    set up a basic log in page and route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,4 +10,16 @@ router.get('/', async (req, res) => {
     }
 });
 
+
+router.get('/login', async (req, res) => {
+  try {
+    res.render('login');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+
 module.exports = router;
