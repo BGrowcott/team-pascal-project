d51e9d7d0c5052c6160142abbc613d0669d757a8
Ben <bgrowcott@hotmail.co.uk>
2022-04-02 15:21:35 +0100

    set up id params

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,28 +13,20 @@ router.get('/', async (req, res) => {
 });
 
 //User page
-router.get('/profile', async (req, res) => {
+router.get('/profile/:id', async (req, res) => {
+
+  const id = req.params.id
   try {
 
-    const userData = await User.findAll();
+    const userData = await User.findByPk(id);
 
-    const users = userData.map((user) => user.get({ plain: true }));
+    const user = userData.get({ plain: true });
 
-    res.render('profile', {users});
+    res.render('profile', {user});
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
-// Log in page
-router.get('/login', async (req, res) => {
-  try {
-    res.render('login');
-    } catch (err) {
-      res.status(500).json(err);
-    }
-});
-
 
 //Forums page
 router.get('/forums', async (req, res) => {
@@ -48,6 +40,15 @@ router.get('/forums', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+// Log in page
+router.get('/login', async (req, res) => {
+  try {
+    res.render('login');
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
 module.exports = router;
