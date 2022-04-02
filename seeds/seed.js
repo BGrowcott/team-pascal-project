10fa1bfef7b16003a36d91ade4adfd4fe14f237d
Ben <bgrowcott@hotmail.co.uk>
2022-04-02 13:44:57 +0100

    set up seeding for tables

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,15 +1,28 @@
 const seedUsers = require('./userSeeds.json');
-const { User } = require('../models');
+const { User, Forum, Comment } = require('../models');
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-    await sequelize.sync({ force: true });
-    console.log('\n----- DATABASE SYNCED -----\n');
-    const users = await User.bulkCreate(seedUsers, {
-        individualHooks: true,
-        returning: true,
-    })
+  await sequelize.sync({ force: true });
+  console.log('\n----- DATABASE SYNCED -----\n');
+  const users = await User.bulkCreate(seedUsers, {
+    individualHooks: true,
+    returning: true,
+  });
 
-}
+  for (const forum of forumData) {
+    await Forum.create({
+      ...forum,
+    });
+  }
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+    });
+  }
+
+  process.exit(0);
+};
 
 seedAll();
