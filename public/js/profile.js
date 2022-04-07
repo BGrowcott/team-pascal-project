70d78bbf34a1343be5db2e06eafdc6200f119de4
Akash Limbu <akashwanem@hotmail.com>
2022-04-07 15:11:00 +0100

    trying to made edit profile button work

---
+++ public/js/profile.js
@@ -0,0 +1,6 @@
+// edit profile button working & save button to save the entered data
+$("#edit-button").click(function () {
+    $(".contact-info").css('display','none');
+    $("#contact-input").removeClass("hide");
+};
+
