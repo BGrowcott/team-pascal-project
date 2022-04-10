9292183e1c761a2810e55b689b989dbe24c29049
Ben <bgrowcott@hotmail.co.uk>
2022-04-10 18:08:13 +0100

    finished setting up aws on the backend with detail protected by dotenv

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
+app.use(express.json({limit: '5mb'}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
